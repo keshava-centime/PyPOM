745e8c6923a328b1a7214c52f4afbcb7ab89ebdd
keshava-centime <71315069+keshava-centime@users.noreply.github.com>
2021-11-05 14:28:00 +0530

    Update selenium_driver.py

--- src/pypom/selenium_driver.py
+++ src/pypom/selenium_driver.py
@@ -5,9 +5,6 @@
 
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import (
-    Android,
-    BlackBerry,
-    Chrome,
     Edge,
     Firefox,
     Ie,
@@ -134,9 +131,6 @@ def register():
             Edge,
             Opera,
             Safari,
-            BlackBerry,
-            PhantomJS,
-            Android,
             Remote,
             EventFiringWebDriver,
         ],
